0499ed7519ee493a34d34fc021c6c22019f40b84
Peter Robertson <peterjdrb@outlook.com>
2019-01-28 11:01:42 +0000

    If response is ever false a message will appear for the user
    #4

--- app.js
+++ app.js
@@ -21,8 +21,12 @@ app.get("/imdb_results", function(req, res) {
     request(url, function(error, response, body){
         if(!error && response.statusCode === 200) {
             var results = JSON.parse(body);
-            console.log(results);
-            res.render("imdb_results", {results:results});
+            if (results.Response === "False"){
+                res.render("imdb_search", {error:results});
+            } else {
+                res.render("imdb_results", {results:results});
+            }
+            
         }
     });
 });
