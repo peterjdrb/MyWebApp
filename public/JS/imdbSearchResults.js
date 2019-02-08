36eb6511b6efba03952c6e7bfc9f150fd2c477ba
Peter Robertson <peterjdrb@outlook.com>
2019-02-08 10:40:10 +0000

    message is returned when no search reuslts
    A message appears where the table would exist to say there is no search reuslts

--- public/JS/imdbSearchResults.js
+++ public/JS/imdbSearchResults.js
@@ -1,18 +1,15 @@
 $( document ).ready(function() {
     
-    // GET REQUEST
     $("#searchSubmit").bind("click", function(){
         clearTableHTML();
-    	ajaxGet();
+    	getSearchResults();
     });
     
     function clearTableHTML (){
-        var table = $("#imdb_results")[0];
-        table.innerHTML = "<tr><th>Poster</th><th>Title</th><th>Year</th><th>Type</th></tr>";
+        $("#imdb_results")[0].innerHTML = "";
     }
     
-    // DO GET
-    function ajaxGet(){
+    function getSearchResults(){
         var name = $("input[name=name]").val();
         var year = $("input[name=year]").val();
         if (year === "") {
@@ -24,25 +21,31 @@ $( document ).ready(function() {
     		type : "GET",
     		url : "/movieSearch/" + name + "/" + year + "/" + category,
     		success: function(result){
+    		    console.log(result);
     		    var table = $("#imdb_results")[0];
-    		    var newRows = "";
-    			$.each(result.Search, function(i, movie){
-    			    var request = "'/imdb_page/" + movie.imdbID + "'";
-    			    var onClickAttr = "window.location.href = " + request;
-    			    var onClickString = "onclick= \"" + onClickAttr;
-    			    
-    			    var rowStart = "<tr " + onClickString + ";\">";
-    			    var posterCell = "<td><img src='" + movie.Poster + "'></td>";
-    			    var titleCell = "<td>" + movie.Title + "</td>";
-    			    var yearCell = "<td>" + movie.Year + "</td>";
-    			    var typeCell = "<td>" + movie.Type + "</td>";
-    			    var rowEnd = "</tr>";
-    			    
-    			    var rowHTML = rowStart + posterCell + titleCell + yearCell + typeCell + rowEnd;
-    			    newRows += rowHTML;
-    			});
-    			
-    			table.innerHTML += newRows;
+    		    if (result.Response === "True") {
+        		    var newRows = "";
+        		    table.innerHTML = "<tr><th>Poster</th><th>Title</th><th>Year</th><th>Type</th></tr>";
+        			$.each(result.Search, function(i, movie){
+        			    var request = "'/imdb_page/" + movie.imdbID + "'";
+        			    var onClickAttr = "window.location.href = " + request;
+        			    var onClickString = "onclick= \"" + onClickAttr;
+        			    
+        			    var rowStart = "<tr " + onClickString + ";\">";
+        			    var posterCell = "<td><img src='" + movie.Poster + "'></td>";
+        			    var titleCell = "<td>" + movie.Title + "</td>";
+        			    var yearCell = "<td>" + movie.Year + "</td>";
+        			    var typeCell = "<td>" + movie.Type + "</td>";
+        			    var rowEnd = "</tr>";
+        			    
+        			    var rowHTML = rowStart + posterCell + titleCell + yearCell + typeCell + rowEnd;
+        			    newRows += rowHTML;
+        			});
+        			
+        			table.innerHTML += newRows;
+        		} else {
+        		    table.innerHTML = "<h1>No Results Found</h1>";
+        		}
     		},
     		error : function(e) {
     			console.log("ERROR: ", e);
